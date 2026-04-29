c0c57a0445fc6962bd34e6b7e5a8e48ad52b309f
David <davik50@gmail.com>
2026-04-29 08:55:15 +0200

    Initial WPF project setup

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace EnglishVoiceTutor.Desktop
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
