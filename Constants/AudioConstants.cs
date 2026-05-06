da548c3c6306f6124640ecbbeeb5876acfa6e601
David Tsiklauri <david.tsiklauri@outlook.com>
2026-05-06 09:30:36 +0200

    Fix audio recording IO namespace

---
+++ Constants/AudioConstants.cs
@@ -0,0 +1,10 @@
+namespace EnglishVoiceTutor.Desktop.Constants;
+
+public static class AudioConstants
+{
+    public const string AppTempFolderName = "EnglishVoiceTutor.Desktop";
+    public const string RecordingFolderName = "Recordings";
+    public const string RecordingFilePrefix = "voice-recording-";
+    public const string WavFileExtension = ".wav";
+    public const string RecordingTimestampFormat = "yyyyMMdd-HHmmss-fff";
+}
