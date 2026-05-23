67ac45acdac7ae294b19043d4d704f518e6a20e0
David Tsiklauri <david.tsiklauri@outlook.com>
2026-05-23 12:56:13 +0200

    Clear account password box after auth success/logout

--- Views/SettingsView.xaml.cs
+++ Views/SettingsView.xaml.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Windows.Controls;
 using System.Windows;
 using EnglishVoiceTutor.Desktop.ViewModels;
@@ -9,6 +10,37 @@ public partial class SettingsView : UserControl
     public SettingsView()
     {
         InitializeComponent();
+        DataContextChanged += OnDataContextChanged;
+        Unloaded += OnUnloaded;
+    }
+
+    private void OnDataContextChanged(object sender, DependencyPropertyChangedEventArgs e)
+    {
+        if (e.OldValue is SettingsViewModel oldViewModel)
+        {
+            oldViewModel.ClearPasswordRequested -= OnClearPasswordRequested;
+        }
+
+        if (e.NewValue is SettingsViewModel newViewModel)
+        {
+            newViewModel.ClearPasswordRequested += OnClearPasswordRequested;
+        }
+    }
+
+    private void OnUnloaded(object sender, RoutedEventArgs e)
+    {
+        if (DataContext is SettingsViewModel viewModel)
+        {
+            viewModel.ClearPasswordRequested -= OnClearPasswordRequested;
+        }
+
+        DataContextChanged -= OnDataContextChanged;
+        Unloaded -= OnUnloaded;
+    }
+
+    private void OnClearPasswordRequested(object? sender, EventArgs e)
+    {
+        AccountPasswordBox.Clear();
     }
 
     private void AccountPasswordBox_OnPasswordChanged(object sender, RoutedEventArgs e)
