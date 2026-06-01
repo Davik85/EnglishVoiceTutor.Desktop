119dd3294c589c16d853feae179680df56adc904
David Tsiklauri <david.tsiklauri@outlook.com>
2026-06-01 20:22:34 +0200

    Restore empty welcome assets folder

--- ViewModels/WelcomeViewModel.cs
+++ ViewModels/WelcomeViewModel.cs
@@ -1,6 +1,5 @@
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
-using EnglishVoiceTutor.Desktop.Constants;
 using EnglishVoiceTutor.Desktop.Localization;
 
 namespace EnglishVoiceTutor.Desktop.ViewModels;
@@ -11,23 +10,13 @@ public partial class WelcomeViewModel : ViewModelBase
     private readonly Action navigateToSettings;
     private readonly AppLocalizedText localizedText;
 
-    public string AppTitle => AppConstants.ShortAppName;
+    public string AppTitle => localizedText.WelcomeTitle;
 
     public string Subtitle => localizedText.WelcomeSubtitle;
-
-    public string HowItWorksTitle => localizedText.WelcomeMvpHowItWorksTitle;
-
-    public string ChooseTopicStep => localizedText.WelcomeMvpChooseTopicStep;
-
-    public string PracticeStep => localizedText.WelcomeMvpPracticeStep;
-
-    public string CorrectionsStep => localizedText.WelcomeMvpCorrectionsStep;
 
     public string StartLessonButtonText => localizedText.WelcomeStartLessonButton;
 
     public string SettingsButtonText => localizedText.WelcomeSettingsButton;
-
-    public string FooterNote => localizedText.WelcomeFooterNote;
 
     [ObservableProperty]
     private string statusMessage = string.Empty;
